ff20107e2988045d4c651ff9f64ac12b1a2e9089
Boitumelo <boitumelotshehla@gmaail.com>
2021-11-26 10:48:05 +0200

    v1.1.0

--- src/components/home.vue
+++ src/components/home.vue
@@ -1,20 +1,25 @@
 <template>
     
-    <div class="auth-wrapper-home">
-        <div class="auth-inner-home">
-          <svg xmlns="http://www.w3.org/2000/svg" style="display: none;">
-            <symbol id="check-circle-fill" fill="currentColor" viewBox="0 0 16 16">
-              <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zm-3.97-3.03a.75.75 0 0 0-1.08.022L7.477 9.417 5.384 7.323a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-.01-1.05z"/>
-            </symbol>
-            <symbol id="info-fill" fill="currentColor" viewBox="0 0 16 16">
-              <path d="M8 16A8 8 0 1 0 8 0a8 8 0 0 0 0 16zm.93-9.412-1 4.705c-.07.34.029.533.304.533.194 0 .487-.07.686-.246l-.088.416c-.287.346-.92.598-1.465.598-.703 0-1.002-.422-.808-1.319l.738-3.468c.064-.293.006-.399-.287-.47l-.451-.081.082-.381 2.29-.287zM8 5.5a1 1 0 1 1 0-2 1 1 0 0 1 0 2z"/>
-            </symbol>
-            <symbol id="exclamation-triangle-fill" fill="currentColor" viewBox="0 0 16 16">
-              <path d="M8.982 1.566a1.13 1.13 0 0 0-1.96 0L.165 13.233c-.457.778.091 1.767.98 1.767h13.713c.889 0 1.438-.99.98-1.767L8.982 1.566zM8 5c.535 0 .954.462.9.995l-.35 3.507a.552.552 0 0 1-1.1 0L7.1 5.995A.905.905 0 0 1 8 5zm.002 6a1 1 0 1 1 0 2 1 1 0 0 1 0-2z"/>
-            </symbol>
+  <div class="auth-wrapper-home">
+    <div class="auth-inner-home">
+      <svg xmlns="http://www.w3.org/2000/svg" style="display: none;">
+        <symbol id="check-circle-fill" fill="currentColor" viewBox="0 0 16 16">
+            <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zm-3.97-3.03a.75.75 0 0 0-1.08.022L7.477 9.417 5.384 7.323a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-.01-1.05z"/>
+        </symbol>
+        <symbol id="info-fill" fill="currentColor" viewBox="0 0 16 16">
+            <path d="M8 16A8 8 0 1 0 8 0a8 8 0 0 0 0 16zm.93-9.412-1 4.705c-.07.34.029.533.304.533.194 0 .487-.07.686-.246l-.088.416c-.287.346-.92.598-1.465.598-.703 0-1.002-.422-.808-1.319l.738-3.468c.064-.293.006-.399-.287-.47l-.451-.081.082-.381 2.29-.287zM8 5.5a1 1 0 1 1 0-2 1 1 0 0 1 0 2z"/>
+        </symbol>
+        <symbol id="exclamation-triangle-fill" fill="currentColor" viewBox="0 0 16 16">
+          <path d="M8.982 1.566a1.13 1.13 0 0 0-1.96 0L.165 13.233c-.457.778.091 1.767.98 1.767h13.713c.889 0 1.438-.99.98-1.767L8.982 1.566zM8 5c.535 0 .954.462.9.995l-.35 3.507a.552.552 0 0 1-1.1 0L7.1 5.995A.905.905 0 0 1 8 5zm.002 6a1 1 0 1 1 0 2 1 1 0 0 1 0-2z"/>
+        </symbol>
           </svg>
             <h3>home</h3>
-               
+               <div v-if="!isLoaded">
+                <div class="d-flex justify-content-center mb-3">
+                  <b-spinner label=""></b-spinner>
+                </div>
+              </div>
+            <div v-if="isLoaded">
                 <div v-if="!isSelected">
                   <form >
                     <div class="form-group">
@@ -98,13 +103,16 @@
                         <div>
                           <b-button variant="success" @click="next">Next</b-button>
                         </div>
+                      </div>
                     </div>
-                </div>
+                
+                
+              </div>
                 
             
             
-        </div>
     </div>
+  </div>
     
 </template>
 
@@ -137,6 +145,7 @@ export default {
               isStudData:null,
               isfilled:null,
               studCount:null,
+              isLoaded:true
         }
     },
     components:{
@@ -146,6 +155,8 @@ export default {
         decode(decodeString){
             this.qrText=decodeString;
             this.getStudentData();
+            this.isLoaded=false;
+            //console.log(decodeString)
         },
         drawOutline(detectedCodes,ctx){
             
@@ -193,6 +204,7 @@ export default {
              this.access="Access Denied";
           }
 
+          this.isLoaded=true;
           
 
         } else {
@@ -204,7 +216,7 @@ export default {
           
           
           this.isfilled=false;
-          
+          this.isLoaded=false;
           const db=getFirestore();
          
           if(this.access=="Access Granted"){
@@ -225,10 +237,12 @@ export default {
               date:"",
             
             });
+            this.isLoaded=true;
             this.qrText=null;
           }
           else{
             this.qrText=null;
+            this.isLoaded=true;
           }
 
           
@@ -241,6 +255,7 @@ export default {
 
           this.select.date=dd+'/'+mm+'/'+yyyy;
           this.isSelected=true;
+          this.isLoaded=false;
 
           const db=getFirestore();
           const docRef = doc(db, "Campus", this.select.depature,this.select.destination,this.select.time);
@@ -248,6 +263,7 @@ export default {
           if (docSnap.exists()) {
               console.log("Document data:", docSnap.data());
               this.studCount=docSnap.data().studCount;
+              this.isLoaded=true;
             } else {
               // doc.data() will be undefined in this case
               console.log("No such document!");
